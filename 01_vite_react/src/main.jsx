8e0afd02f8ed4c13979282243817a8b551b76e0c
hironmoy-ray <hpray35@gmail.com>
2024-09-19 00:23:48 +0600

    React Render vs Custom Render Function

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import React from 'react'
 
+// const myElement = (
+//   <a href="https://google.com" target="_blank">
+//     Go to Google
+//   </a>
+// );
+// createRoot(document.getElementById("root")).render(myElement);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const reactElement = React.createElement("a",{href:"https://google.com", target:"_blank"},"Click Google") 
+
+createRoot(document.getElementById("root")).render(reactElement);
